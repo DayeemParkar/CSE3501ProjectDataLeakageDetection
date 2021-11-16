665bc11bb85894090de3e8357658ce35f1e74c07
Dayeem Parkar <70463788+DayeemParkar@users.noreply.github.com>
2021-11-16 21:08:10 +0530

    Update config.php

--- user/config.php
+++ user/config.php
@@ -6,10 +6,10 @@
  *dbPass = Password of your DataBase
  *dbName = Name of your DataBase
  **********************************************************************/
-$dbHost = 'localhost';
-$dbUser = 'root';
-$dbPass = '';
-$dbName = 'dataleakage';
+$dbHost = 'sql5.freemysqlhosting.net';
+$dbUser = 'sql5451502';
+$dbPass = '6NrWzphBie';
+$dbName = 'sql5451502';
 $dbC = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName)
         or die('Error Connecting to MySQL DataBase');
 ?>
