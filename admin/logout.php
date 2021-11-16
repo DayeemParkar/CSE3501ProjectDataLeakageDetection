b91f2521fe5b0380b364905badf204a1aaf80a90
Dayeem Parkar <70463788+DayeemParkar@users.noreply.github.com>
2021-11-16 19:13:02 +0530

    Update logout.php

--- admin/logout.php
+++ admin/logout.php
@@ -1,5 +1,5 @@
 <?php
 session_start(); //Start the current session
 session_destroy(); //Destroy it! So we are logged out now
-header("location:http://localhost/data-leakage-detection/index.php?msg=Successfully Logged out"); // Move back to login.php with a logout message
+header("https://cse3501project.herokuapp.com?msg=Successfully Logged out"); // Move back to login.php with a logout message
 ?>
