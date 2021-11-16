4a6949e42166c8c9db17524d8abe326e1be2796b
Dayeem Parkar <70463788+DayeemParkar@users.noreply.github.com>
2021-11-16 19:39:13 +0530

    Update sendkeytouser.php

--- admin/sendkeytouser.php
+++ admin/sendkeytouser.php
@@ -9,12 +9,12 @@ $a3 = $_POST['a3'];
 
 
 
- $con = mysqli_connect("localhost","root","");
+ $con = mysqli_connect("sql5.freemysqlhosting.net","sql5451502","6NrWzphBie");
                                 if (!$con)
                                     echo('Could not connect: ' . mysqli_error());
                                 else
                                 {
-                                    mysqli_select_db( $con,"dataleakage");
+                                    mysqli_select_db( $con,"sql5451502");
 	$sql = "UPDATE askkey SET k='$a2',status='yes' WHERE filename='$a3'and user='$a1' ";
     $result = mysqli_query($con,$sql) or die ("Could not send data into DB: " . mysqli_error($con));
     $sql = "INSERT INTO record(subject,topic,sendto,time) VALUES ('" . $_POST["a3"] ."','" . $_POST["a2"] . "','" . 
@@ -24,7 +24,7 @@ $a3 = $_POST['a3'];
 						else
 							echo '<script language="javascript">alert("Key has been successfully send!")</script>';
 						
-	header("Location: admin.php");
+	header("Location: https://cse3501project.herokuapp.com/admin/admin.php");
 	exit;
 								}
 ?>
