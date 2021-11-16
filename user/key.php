a751e7314f2d8f6db727649e1d1b9c2ef9f47598
Dayeem Parkar <70463788+DayeemParkar@users.noreply.github.com>
2021-11-16 21:10:59 +0530

    Update key.php

--- user/key.php
+++ user/key.php
@@ -12,16 +12,16 @@ $id = $_GET['id'];
 
 
 
- $con = mysqli_connect("localhost","root","");
+ $con = mysqli_connect("sql5.freemysqlhosting.net","sql5451502","6NrWzphBie");
                                 if (!$con)
                                     echo('Could not connect: ' . mysqli_error());
                                 else
                                 {
-                                    mysqli_select_db($con,"dataleakage" );
+                                    mysqli_select_db($con,"sql5451502" );
 	$sql = "insert into askkey (user, filename, status, reciver,k) values ('$_SESSION[name]','$id','no','admin','')";
 	$result = mysqli_query($con,$sql) or die ("Could not insert data into DB: " . mysqli_error());
 	//echo '<script language="javascript">alert("Thank You!! for asking for key")</script>';
-	header("Location: user.php");
+	header("Location: https://cse3501project.herokuapp.com/user/user.php");
 	exit;
 								}
 ?>
