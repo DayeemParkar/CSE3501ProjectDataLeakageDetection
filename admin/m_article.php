f9d0eb15b54560afe35a0beaea015762dfd56a47
Dayeem Parkar <70463788+DayeemParkar@users.noreply.github.com>
2021-11-16 19:17:58 +0530

    Update m_article.php

--- admin/m_article.php
+++ admin/m_article.php
@@ -2,25 +2,18 @@
 // Start a session for error reporting
 session_start();
 
-
-
-
-
 // Get our POSTed variables
 $id = $_GET['id'];
 
-
-
-
- $con = mysqli_connect("localhost","root","");
+ $con = mysqli_connect("sql5.freemysqlhosting.net","sql5451502","6NrWzphBie");
                                 if (!$con)
                                     echo('Could not connect: ' . mysqli_error());
                                 else
                                 {
-                                    mysqli_select_db( $con,"dataleakage");
+                                    mysqli_select_db( $con,"sql5451502");
 	$sql = "delete from presentation where subject='$id'";
 	$result = mysqli_query($con,$sql) or die ("Could not insert data into DB: " . mysqli_error());
-	header("Location: admin.php");
+	header("Location: https://cse3501project.herokuapp.com/admin/admin.php");
 	exit;
 								}
 ?>
