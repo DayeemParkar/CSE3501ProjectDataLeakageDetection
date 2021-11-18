c13bed650849f14008ad286356fa4aa48ad00a97
Dayeem Parkar <70463788+DayeemParkar@users.noreply.github.com>
2021-11-18 17:44:57 +0530

    Update register.php

--- register.php
+++ register.php
@@ -15,11 +15,10 @@ $pass = $_POST['pass'];
 $email=$_POST['email'];
 
 	$sql = "insert into register ( username, userid, password, emailid) values ('$uname', '$uid','$pass', '$email')";
-	$result = mysqli_query($conn, $sql) or die ("Could not insert data into DB: " . mysqli_error($conn));
+	/*$result = mysqli_query($conn, $sql) or die ("Could not insert data into DB: " . mysqli_error($conn));
 	$sql = "insert into leaker ( id, name, probability) values ('$uid', '$uname', 0.0)";
 	$result = mysqli_query($conn, $sql) or die ("Could not insert data into DB: " . mysqli_error($conn));
-	header("Location:https://cse3501project.herokuapp.com/userlogin.php");
-	exit;
+	header("Location:https://cse3501project.herokuapp.com/userlogin.php");*/
 
 ?>
 </body>
