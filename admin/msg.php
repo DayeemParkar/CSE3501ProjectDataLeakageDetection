1aaebf3b9f4e9747c488b70136df8f9171ca8929
Dayeem Parkar <70463788+DayeemParkar@users.noreply.github.com>
2021-11-23 15:21:29 +0530

    Update msg.php

--- admin/msg.php
+++ admin/msg.php
@@ -13,12 +13,12 @@ $a3 = $_POST['a3'];
 
 
 
- $con = mysqli_connect("sql5.freemysqlhosting.net","sql5451502","6NrWzphBie");
+ $con = mysqli_connect("sql6.freemysqlhosting.net","sql6453269","13gSHbLs1b");
                                 if (!$con)
                                     echo('Could not connect: ' . mysqli_error());
                                 else
                                 {
-                                    mysqli_select_db( $con,"sql5451502");
+                                    mysqli_select_db( $con,"sql6453269");
 	$sql = "insert into msg (sender, email, reciver, msg) values ('$_SESSION[name]', '$a1','$a2', '$a3')";
 	$result = mysqli_query($con,$sql) or die ("Could not insert data into DB: " . mysqli_error());
 	header("Location: https://cse3501project.herokuapp.com/admin/admin.php");
