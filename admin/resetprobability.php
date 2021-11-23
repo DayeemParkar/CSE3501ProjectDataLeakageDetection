7a9cb6cd2a6e8c785ab97fd994005cfacbcc9e13
Dayeem Parkar <70463788+DayeemParkar@users.noreply.github.com>
2021-11-23 15:25:09 +0530

    Update resetprobability.php

--- admin/resetprobability.php
+++ admin/resetprobability.php
@@ -1,12 +1,12 @@
 <?php 
 session_start();
 
-$con = mysqli_connect("sql5.freemysqlhosting.net","sql5451502","6NrWzphBie");
+$con = mysqli_connect("sql6.freemysqlhosting.net","sql6453269","13gSHbLs1b");
 
 if (!$con)
     echo('Could not connect: ' . mysqli_error());
 else {
-    mysqli_select_db($con,"sql5451502" );
+    mysqli_select_db($con,"sql6453269" );
     $sql = "UPDATE leaker SET probability=0.0 WHERE 1=1 ";
     $result = mysqli_query($con,$sql) or die ("Could not send data into DB: " . mysqli_error($con));
     header("Location: https://cse3501project.herokuapp.com/admin/leakfile.php");
