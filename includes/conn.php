ea88ad7a866a8af6154567a1eba9ccca8e8489a7
Dayeem Parkar <70463788+DayeemParkar@users.noreply.github.com>
2021-11-23 15:03:54 +0530

    Update conn.php

--- includes/conn.php
+++ includes/conn.php
@@ -1,16 +1,16 @@
 <?php
 
 // Host name
-$host = "sql5.freemysqlhosting.net";
+$host = "sql6.freemysqlhosting.net";
 
 // Database username
-$username = "sql5451502";
+$username = "sql6453269";
 
 // Database password
-$password = "6NrWzphBie";
+$password = "13gSHbLs1b";
 
 // Name of database
-$database = "sql5451502";
+$database = "sql6453269";
 
 $conn = mysqli_connect($host, $username, $password) or die ("Could not connect");
 $db = mysqli_select_db($conn, $database) or die ("Could not select DB");
