71d43681983d887b3f7c2f6675984fc82ef910a6
Dayeem Parkar <70463788+DayeemParkar@users.noreply.github.com>
2021-11-23 15:22:42 +0530

    Update presentationD.php

--- admin/presentationD.php
+++ admin/presentationD.php
@@ -10,12 +10,12 @@
                         <?php
                             {
                                 $row="";
-                                $con = mysqli_connect("sql5.freemysqlhosting.net","sql5451502","6NrWzphBie");
+                                $con = mysqli_connect("sql6.freemysqlhosting.net","sql6453269","13gSHbLs1b");
                                 if (!$con)
                                     echo('Could not connect: ' . mysqli_error());
                                 else
                                 {
-                                    mysqli_select_db($con,"sql5451502");
+                                    mysqli_select_db($con,"sql6453269");
                                     $sql = 'SELECT subject, topic, fname FROM presentation';
                                     $retval = mysqli_query(  $con,$sql );
                                     if(! $retval )
