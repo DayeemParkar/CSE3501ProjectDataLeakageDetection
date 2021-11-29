32e1e6ad33376d465e4d4a3a8455c0a9343b568f
Dayeem Parkar <70463788+DayeemParkar@users.noreply.github.com>
2021-11-29 11:43:43 +0530

    Update detail1.php

--- user/detail1.php
+++ user/detail1.php
@@ -1,12 +1,14 @@
 <?php 
+include 'config.php';
+
 session_start();
 
-$con = mysqli_connect("sql6.freemysqlhosting.net","sql6453269","13gSHbLs1b");
+//$con = mysqli_connect("sql6.freemysqlhosting.net","sql6453269","13gSHbLs1b");
 
 if (!$con)
     echo('Could not connect: ' . mysqli_error());
 else {
-    mysqli_select_db($con,"sql6453269" );
+    //mysqli_select_db($con,"sql6453269" );
     $k1=$_POST["s1"];
     $k2=$_POST["s2"];
 
