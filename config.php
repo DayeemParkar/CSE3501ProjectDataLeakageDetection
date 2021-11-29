3cd5908f137ae758338d72cb01a15bf5a55a94d7
Dayeem Parkar <70463788+DayeemParkar@users.noreply.github.com>
2021-11-29 11:18:07 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,6 +3,6 @@ $dbHost = 'www.db4free.net';
 $dbUser = 'cse3501';
 $dbPass = 'cse3501p';
 $dbName = 'cse3501p';
-$dbC = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName)
+$con = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName)
         or die('Error Connecting to MySQL DataBase');
 ?>
