3fa0db11831a2d6d735a3c3d0601ecf40ca203db
Dayeem Parkar <70463788+DayeemParkar@users.noreply.github.com>
2021-11-29 11:26:44 +0530

    Update d_user.php

--- admin/d_user.php
+++ admin/d_user.php
@@ -2,9 +2,7 @@
 // Start a session for error reporting
 session_start();
 
-
-
-
+include 'config.php';
 
 // Get our POSTed variables
 $id = $_GET['id'];
@@ -12,12 +10,11 @@ $id = $_GET['id'];
 
 
 
- $con = mysqli_connect("sql6.freemysqlhosting.net","sql6453269","13gSHbLs1b");
+ //$con = mysqli_connect("sql6.freemysqlhosting.net","sql6453269","13gSHbLs1b");
                                 if (!$con)
                                     echo('Could not connect: ' . mysqli_error());
                                 else
                                 {
-                                    mysqli_select_db($con,"sql6453269" );
 	$sql = "delete from register where username='$id'";
 	$result = mysqli_query($con,$sql) or die ("Could not insert data into DB: " . mysqli_error());
 	header("Location: https://cse3501project.herokuapp.com/admin/admin.php");
