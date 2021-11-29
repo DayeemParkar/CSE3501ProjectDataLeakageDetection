1bdfb1efb850d4e4bc2b93679d42064774f2692d
Dayeem Parkar <70463788+DayeemParkar@users.noreply.github.com>
2021-11-29 11:44:30 +0530

    Update key.php

--- user/key.php
+++ user/key.php
@@ -1,5 +1,7 @@
 <?php
 // Start a session for error reporting
+include 'config.php';
+
 session_start();
 
 
@@ -12,12 +14,12 @@ $id = $_GET['id'];
 
 
 
- $con = mysqli_connect("sql6.freemysqlhosting.net","sql6453269","13gSHbLs1b");
+ //$con = mysqli_connect("sql6.freemysqlhosting.net","sql6453269","13gSHbLs1b");
                                 if (!$con)
                                     echo('Could not connect: ' . mysqli_error());
                                 else
                                 {
-                                    mysqli_select_db($con,"sql6453269" );
+                                    //mysqli_select_db($con,"sql6453269" );
 	$sql = "insert into askkey (user, filename, status, reciver,k) values ('$_SESSION[name]','$id','no','admin','')";
 	$result = mysqli_query($con,$sql) or die ("Could not insert data into DB: " . mysqli_error());
 	//echo '<script language="javascript">alert("Thank You!! for asking for key")</script>';
