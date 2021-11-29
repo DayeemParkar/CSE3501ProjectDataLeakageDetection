72c97eede2cd8826c03e6b36d375c1b6206b5021
Dayeem Parkar <70463788+DayeemParkar@users.noreply.github.com>
2021-11-29 11:36:15 +0530

    Update sendkeytouser.php

--- admin/sendkeytouser.php
+++ admin/sendkeytouser.php
@@ -1,5 +1,7 @@
 <?php
 // Start a session for error reporting
+include 'config.php';
+
 session_start();
 // Get our POSTed variables
 $a1 = $_POST['a1'];
@@ -9,12 +11,12 @@ $a3 = $_POST['a3'];
 
 
 
- $con = mysqli_connect("sql6.freemysqlhosting.net","sql6453269","13gSHbLs1b");
+ //$con = mysqli_connect("sql6.freemysqlhosting.net","sql6453269","13gSHbLs1b");
                                 if (!$con)
                                     echo('Could not connect: ' . mysqli_error());
                                 else
                                 {
-                                    mysqli_select_db( $con,"sql6453269");
+                                    //mysqli_select_db( $con,"sql6453269");
 	$sql = "UPDATE askkey SET k='$a2',status='yes' WHERE filename='$a3'and user='$a1' ";
     $result = mysqli_query($con,$sql) or die ("Could not send data into DB: " . mysqli_error($con));
     $sql = "INSERT INTO record(subject,topic,sendto,time) VALUES ('" . $_POST["a3"] ."','" . $_POST["a2"] . "','" . 
