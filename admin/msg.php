bf840dc4f844e4471b68a739bb56cfd53e59ef7a
Dayeem Parkar <70463788+DayeemParkar@users.noreply.github.com>
2021-12-04 21:06:48 +0530

    Update msg.php

--- admin/msg.php
+++ admin/msg.php
@@ -4,26 +4,28 @@ include 'config.php';
 
 session_start();
 
-
-
-
-
 // Get our POSTed variables
 $a1 = $_POST['a1'];
 $a2 = $_POST['a2'];
 $a3 = $_POST['a3'];
 
+$a2 = mysqli_real_escape_string($conn, $a2);
+$a3 = mysqli_real_escape_string($conn, $a3);
 
-
- //$con = mysqli_connect("sql6.freemysqlhosting.net","sql6453269","13gSHbLs1b");
-                                if (!$con)
-                                    echo('Could not connect: ' . mysqli_error());
-                                else
-                                {
-                                    //mysqli_select_db( $con,"sql6453269");
-	$sql = "insert into msg (sender, email, reciver, msg) values ('$_SESSION[name]', '$a1','$a2', '$a3')";
-	$result = mysqli_query($con,$sql) or die ("Could not insert data into DB: " . mysqli_error());
-	header("Location: https://cse3501project.herokuapp.com/admin/admin.php");
-	exit;
-								}
+if (!$con)
+	echo('Could not connect: ' . mysqli_error());
+else
+{
+	if ($a1 contains '<' || $a2 contains '<' || $a3 contains '<')
+	{
+		echo "<script>alert('Invalid message content');</script>";
+	}
+	else
+	{
+		$sql = "insert into msg (sender, email, reciver, msg) values ('$_SESSION[name]', '$a1','$a2', '$a3')";
+		$result = mysqli_query($con,$sql) or die ("Could not insert data into DB: " . mysqli_error());
+		header("Location: https://cse3501project.herokuapp.com/admin/admin.php");
+		exit;
+	}
+}
 ?>
